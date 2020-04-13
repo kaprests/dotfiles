9740237b76dba52ee8c9f71a172a792077bdd098
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-04-13 21:09:20 +0200

    Better vulkan SDK comment

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,8 +1,8 @@
 # Adds Julia to path
 set -gx PATH ~/bin/julia-1.4.0/bin/ $PATH
 
-# Vulkan env. var
-set -gx VULKAN_SDK ~/vulkan/1.2.135.0/x86_64
+# Vulkan SDK setup
+set -gx VULKAN_SDK ~/VulkanSDK/1.2.135.0/x86_64
 set -gx PATH $VULKAN_SDK/bin $PATH
 set -gx LD_LIBRARY_PATH $VULKAN_SDK/lib
 set -gx VK_LAYER_PATH $VULKAN_SDK/etc/vulkan/explicit_layer.d
