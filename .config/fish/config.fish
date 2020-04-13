e069431d3f8195481aa2d7e53bfc8510221c029d
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-04-13 05:21:03 +0200

    Updates fish and vim dotfiles

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,12 @@
 # Adds Julia to path
 set -gx PATH ~/bin/julia-1.4.0/bin/ $PATH
 
+# Vulkan env. var
+set -gx VULKAN_SDK ~/vulkan/1.2.135.0/x86_64
+set -gx PATH $VULKAN_SDK/bin $PATH
+set -gx LD_LIBRARY_PATH $VULKAN_SDK/lib
+set -gx VK_LAYER_PATH $VULKAN_SDK/etc/vulkan/explicit_layer.d
+
+
 # Set dotfiles alias 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
--- .vimrc
+++ .vimrc
@@ -4,6 +4,8 @@ set paste " paste text without formatting funkyness "
 set ignorecase " case insensitive search "
 set autoindent " keep current indentation on newline "
 set tabstop=4 " tabs as 4 spaces "
+set shiftwidth=4
+set expandtab
 set hlsearch " highlight search matches "
 set wildmenu " command suggestion upon tab "
 
