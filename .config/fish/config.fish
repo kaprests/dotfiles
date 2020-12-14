dabfc61d574bf1d3f01b6c0bc0356ceccb1b2219
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2020-12-14 16:42:37 +0100

    Had some changes, not bothered to check what.

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -7,7 +7,5 @@ set -gx PATH $VULKAN_SDK/bin $PATH
 set -gx LD_LIBRARY_PATH $VULKAN_SDK/lib
 set -gx VK_LAYER_PATH $VULKAN_SDK/etc/vulkan/explicit_layer.d
 
-
 # Set dotfiles alias 
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
-
