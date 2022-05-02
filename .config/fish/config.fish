3abf3e2eac9a3cc64ac1077cc169f51be2e376e5
Knut Andre Grytting Prestsveen <kaprests@gmail.com>
2022-05-02 14:20:27 +0200

    New bar

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,11 +2,14 @@
 alias dotfiles='/usr/bin/git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME'
 #alias vim='nvim'
 
+# Basic config
 set -U fish_user_paths $fish_user_paths $HOME/.local/bin
 set fish_greeting                       # Supresses fish intro message
 set TERM "xterm-256color"               # Sets the terminal type
+fish_vi_key_bindings                    # vi mode
 
-fish_vi_key_bindings # vi mode
+# Path
+fish_add_path $HOME/.emacs.d/bin
 
 function fish_user_key_bindings
     switch $TERM
